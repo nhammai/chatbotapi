623793a8a3fbaa80b1532b091a674c95e0c4b56c
nhammai <104519047+nhammai@users.noreply.github.com>
2023-11-04 07:30:58 +0000

    change the prompt to make it answer better

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import json
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 # Call the openai ChatCompletion endpoint, with th ChatGPT model
-response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                        messages=[{
-                                            "role": "user",
-                                            "content": "Hello World!"
-                                        }])
+# response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+#                                         messages=[{
+#                                             "role": "user",
+#                                             "content": "Hello World!"
+#                                         }])
 
-# Extract the response
-print(response['choices'][0]['message']['content'])
+# # Extract the response
+# print(response['choices'][0]['message']['content'])
 
 app = Flask(__name__)
 CORS(app)
@@ -31,14 +31,22 @@ def ask_gpt3(prompt):
       "role":
       "system",
       "content":
-      "You're an AI assistant who answers user questions from the dataset."
+      "You're a helpful assistant that can help people have their best knowledge and have great experience"
   }, {
+    "role": "user",
+    "content": "Act like you're an expert know alots about geography. First tell them about yourself. You can answer questions about countries, capitals, languages, currency, population, climate. Also you can help people answer their question about where should they travel. You're so funny and friendly. And you will say it in a nice format. If the people ask you about another topic that you don't know about just act friendly and talk about what you know. But most of the time you will focus on the topic you know. Only speak in only 1 sentence. And be precisely and to point about what you say. To the point don't ramble. Now the text bellow is from the people: "
+  },
+                                        
+
+
+                                                    
+                                                    {
       "role": "user",
       "content": prompt
   }],
                                           model='gpt-3.5-turbo',
                                           temperature=0.7,
-                                          max_tokens=100,
+                                          
                                           n=1,
                                           stop=None,
                                           frequency_penalty=0,
@@ -58,6 +66,7 @@ def ask():
   if not prompt:
     return 'No prompt provided', 400
   answer = ask_gpt3(prompt)
+  print(answer)
   return jsonify({'answer': answer})
 
 
